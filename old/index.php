e1f59c1763bb143276f4611f28b99f9dc64f789b
Prital Patel <baldhapiyu@gmail.com>
2017-08-20 09:39:14 +0200

    Init

---
+++ old/index.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<meta name="viewport" content="width=device-width, initial-scale=1">
+	<!-- The above 3 meta tags *must* come first in the head; any other head content must come *after* these tags -->
+	<meta name="description" content="Aeon Polymer, Ahmedabad">
+	<meta name="author" content="Gunjan Patel">
+
+	<title>Aeon Polymer</title>
+
+	<!-- Bootstrap core CSS -->
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/css/bootstrap.min.css">
+
+	<!-- Custom styles for this template -->
+	<link href="sticky-footer.css" rel="stylesheet">
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-65301392-2', 'auto');
+	  ga('send', 'pageview');
+
+	</script>
+  </head>
+
+  <body>
+
+	<!-- Begin page content -->
+	<div class="container">
+	  <div class="page-header">
+		<h1>Aeon Polymer</h1>
+	  </div>
+	  <p class="lead">Stay connected...</p>
+	  <p>Excited to know about us. <a href="mailto:info@aeonpolymer.com">Contact Here!</a></p>
+	</div>
+
+	<footer class="footer">
+	  <div class="container">
+			<p class="text-muted x-small">
+				<span>
+					<span class="glyphicon glyphicon-copyright-mark"></span> 2015 <a href="http://aeonpolymer.com">aeonpolymer.com</a>
+				</span>
+				<span class="pull-right">
+					Developer with <span class="glyphicon glyphicon-heart"></span> by <a href="https://twitter.com/ergunjanpatel">@ergunjanpatel</a>.
+				</span>
+			</p>
+	  </div>
+	</footer>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.11.3/jquery.min.js"></script>
+	<script type="text/javascript">
+	jQuery(document).ready(function($) {
+		jQuery("a:contains('Linux Reseller Hosting')").remove();
+		jQuery("a:contains('Web Hosting')" ).remove();
+	});
+	</script>
+  </body>
+</html>
